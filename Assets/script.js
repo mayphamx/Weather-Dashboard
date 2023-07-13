ec06d890100b828e48c023956719fa938f9efc5f
May <maypham@utexas.edu>
2023-07-12 23:49:02 -0500

    minor changes in js and added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,61 +1,25 @@
-// var for the city name api URL with parameters q (for the city variable) and appid (for the api key)
-// Possible example: var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-// search button click event listener
-    // fetch function to get API
-    // We'll want to get the cities lat and log
-
-    // function to save user search to local storage
-    
-    // dayjs to render weather by date/time accuracy on the page
-    // api link for each city
-    
-    // var link lat and long
-    
-    // var cityURL 
-    // user inputs city it goes into cityURL
-    // from cityURL we'll take the lat and long using 
-
-
-// function to render it to the page
-// to retrieve geographical coordinates given a city name using 5-day forecast
-// fetch request gets a list of all the repos for the node.js organization
-
-// link ...
-// paraments
-// q = city
-// appid = keyAPI
-
-// var city
-
-// linky here
-
-// var cityURL 
-// parament q and appid
-// variable for city
-// user inputs city it goes into cityURL
-// from cityURL we'll take the lat and long using 
-// collect city choice of user input
-
-// api key & api by the lat and long coordinate & api by city input
-// var coordinateAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=efb07af6a99214059c7511ed2b15133e";
-// var cityAPI = "https://api.openweathermap.org/data/2.5/forecast?q={cityname}&appid=efb07af6a99214059c7511ed2b15133e";
-
 var APIKey = "efb07af6a99214059c7511ed2b15133e";
+// id -> variables
 var searchButton = document.getElementById('search-button');
 var listHistory = document.getElementById('history');
 var currentDayList = document.getElementById("currentDay");
+var day1List = document.getElementById("day1");
+var day2List = document.getElementById("day2");
+var day3List = document.getElementById("day3");
+var day4List = document.getElementById("day4");
+var day5List = document.getElementById("day5");
 
+// click events to perform per function
 searchButton.addEventListener('click', getCity);
 searchButton.addEventListener('click', searchCity);
 
+// local storage to save user inputs
 var userSearches = [];
 
-function searchCity() {
+function searchCity() {wd
   var searchButton = document.getElementById('userInput').value;
   userSearches.push(searchButton);
   localStorage.setItem("SearchHistory", JSON.stringify(userSearches));
-  console.log(userSearches);
 }
 
 function searchHistory() {
@@ -74,6 +38,7 @@ function searchHistory() {
 }
 searchHistory()
 
+// fetch city API to save lat and lon data as variables
 function getCity() {
   var inputCity = document.getElementById('userInput').value;
   //console.log(userInput);
@@ -90,26 +55,32 @@ function getCity() {
       var lat = (data.city.coord.lat);
       var lon =(data.city.coord.lon);
       console.log(lat,lon);
+      // use lat and lon variables to call getcoor function to run
       getCoor(lat,lon);
   })
 };
 
+// use lat and lon as parameters to fetch base API
 function getCoor(lat, lon) {
   var coordinateAPI = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=efb07af6a99214059c7511ed2b15133e";
 
+  // currentDay
+  // fetch base API
   fetch(coordinateAPI)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      // var -> get id=cityname (header 4)
       var place = document.getElementById("cityname");
+      // var -> create as list element
       var temp = document.createElement("li");
       var humidity = document.createElement("li");
       var descr = document.createElement("li");
       var wind= document.createElement("li");
       var date = document.createElement("li");
-      
+      // var -> text content using dot method in data/consol
       place.textContent = (data.city.name);
       temp.textContent = "Temperature: "+(data.list[0].main.temp)+ "k";
       humidity.textContent = "Humidity: " +(data.list[0].main.humidity)+ "%";
@@ -117,46 +88,140 @@ function getCoor(lat, lon) {
       wind.textContent = "Wind: "+(data.list[0].wind.speed)+ "mph";
       date.textContent = "Date: "+(data.list[0].dt_txt);
       
-      // var li = document.createElement("button");
+      // append to ID current day
       currentDayList.appendChild(place);
       currentDayList.appendChild(temp);
       currentDayList.appendChild(humidity);
       currentDayList.appendChild(descr);
       currentDayList.appendChild(wind);
       currentDayList.appendChild(date);
-      console.log(temp,humidity,wind,date,place);
+    })
+
+    // Day 1 - tmmrw
+  fetch(coordinateAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var temp1 = document.createElement("li");
+      var humidity1 = document.createElement("li");
+      var descr1 = document.createElement("li");
+      var wind1= document.createElement("li");
+      var date1 = document.createElement("li");
+      
+      temp1.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      temp1.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      humidity1.textContent = "Humidity: " +(data.list[10].main.humidity)+ "%";
+      descr1.textContent = "Forecast: "+(data.list[10].weather[0].description);
+      wind1.textContent = "Wind: "+(data.list[10].wind.speed)+ "mph";
+      date1.textContent = "Date: "+(data.list[10].dt_txt);
+      
+      day1List.appendChild(date1);
+      day1List.appendChild(temp1);
+      day1List.appendChild(humidity1);
+      day1List.appendChild(descr1);
+      day1List.appendChild(wind1);
+    })
+
+    // Day 2
+  fetch(coordinateAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var temp2 = document.createElement("li");
+      var humidity2 = document.createElement("li");
+      var descr2 = document.createElement("li");
+      var wind2= document.createElement("li");
+      var date2 = document.createElement("li");
+      
+      temp2.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      temp2.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      humidity2.textContent = "Humidity: " +(data.list[10].main.humidity)+ "%";
+      descr2.textContent = "Forecast: "+(data.list[10].weather[0].description);
+      wind2.textContent = "Wind: "+(data.list[10].wind.speed)+ "mph";
+      date2.textContent = "Date: "+(data.list[10].dt_txt);
+      
+      day2List.appendChild(date2);
+      day2List.appendChild(temp2);
+      day2List.appendChild(humidity2);
+      day2List.appendChild(descr2);
+      day2List.appendChild(wind2);
+      
+    })
+    // Day 3
+  fetch(coordinateAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var temp3 = document.createElement("li");
+      var humidity3 = document.createElement("li");
+      var descr3 = document.createElement("li");
+      var wind3= document.createElement("li");
+      var date3 = document.createElement("li");
+      
+      temp3.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      temp3.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      humidity3.textContent = "Humidity: " +(data.list[10].main.humidity)+ "%";
+      descr3.textContent = "Forecast: "+(data.list[10].weather[0].description);
+      wind3.textContent = "Wind: "+(data.list[10].wind.speed)+ "mph";
+      date3.textContent = "Date: "+(data.list[10].dt_txt);
+      
+      day3List.appendChild(date3);
+      day3List.appendChild(temp3);
+      day3List.appendChild(humidity3);
+      day3List.appendChild(descr3);
+      day3List.appendChild(wind3);
+    })
+    // Day 4
+  fetch(coordinateAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var temp4 = document.createElement("li");
+      var humidity4 = document.createElement("li");
+      var descr4 = document.createElement("li");
+      var wind4= document.createElement("li");
+      var date4 = document.createElement("li");
+      
+      temp4.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      temp4.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      humidity4.textContent = "Humidity: " +(data.list[10].main.humidity)+ "%";
+      descr4.textContent = "Forecast: "+(data.list[10].weather[0].description);
+      wind4.textContent = "Wind: "+(data.list[10].wind.speed)+ "mph";
+      date4.textContent = "Date: "+(data.list[10].dt_txt);
+      
+      day4List.appendChild(date4);
+      day4List.appendChild(temp4);
+      day4List.appendChild(humidity4);
+      day4List.appendChild(descr4);
+      day4List.appendChild(wind4);
+    })
+    // Day 5
+  fetch(coordinateAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var temp5 = document.createElement("li");
+      var humidity5 = document.createElement("li");
+      var descr5 = document.createElement("li");
+      var wind5= document.createElement("li");
+      var date5 = document.createElement("li");
+      
+      temp5.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      temp5.textContent = "Temperature: "+(data.list[10].main.temp)+ "k";
+      humidity5.textContent = "Humidity: " +(data.list[10].main.humidity)+ "%";
+      descr5.textContent = "Forecast: "+(data.list[10].weather[0].description);
+      wind5.textContent = "Wind: "+(data.list[10].wind.speed)+ "mph";
+      date5.textContent = "Date: "+(data.list[10].dt_txt);
+      
+      day5List.appendChild(date5);
+      day5List.appendChild(temp5);
+      day5List.appendChild(humidity5);
+      day5List.appendChild(descr5);
+      day5List.appendChild(wind5);
     })
 }; 
-
-
-// var city;
-
-// function citydata() {
-//   // fetch API
-  // fetch(cityAPI, {
-  //   lat:'data[i].lat';
-  //   lon:'data[i].lon';
-  // })
-  //   .then(function (response) {
-  //     return response.json();
-  //   })
-  //   .then(function (data) {
-  //     console.log('Lat/Lon \n----------');
-  //     console.log(data);
-//     // retrieve lat and lon from json API response
-      // for (var i = 0; i < data.length; i++) {
-      //   console.log(data[i].lon);
-      //   console.log(data[i].lat);
-        // var lat = document.createElement('li');
-        // var long = document.createElement('li');
-        // lat.textContent = data[i].lat;
-        // long.textContent = data[i].html_;
-        // apiLat.append(lat);
-        // apiLat.append(long);
-        // console.log(lat.textContent);
-        // console.log(long.textContent);
-//     }
-//   })
-// };
-
-// searchButton.addEventListener('click', getApi);
